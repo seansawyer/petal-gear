dc29ef28881d478ad01322aac13438d47ebf3c34
Sean Sawyer <seancsawyer@gmail.com>
2021-01-12 22:53:43 -0500

    Field of view implementation

--- engine.py
+++ engine.py
@@ -1,7 +1,8 @@
 from typing import Set, Iterable
 
-from tcod.context import Context
 from tcod.console import Console
+from tcod.context import Context
+from tcod.map import compute_fov
 
 from entity import Entity
 from game_map import GameMap
@@ -16,6 +17,8 @@ class Engine:
         self.player = player
         self.game_map = game_map
         self.event_handler = EventHandler()
+        # Compute an initial field of view before first render
+        self.update_fov()
 
     def handle_events(self, events: Iterable) -> None:
         for event in events:
@@ -23,10 +26,23 @@ class Engine:
             if action is None:
                 continue
             action.perform(self, self.player)
+            # Update field of view to reflect a changed position
+            self.update_fov()
+
+    def update_fov(self) -> None:
+        """Recompute the field of view based on the player's position"""
+        player_pos = self.player.x, self.player.y
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"], player_pos, radius=8
+        )
+        # Add any tiles the player has seen to her memory
+        self.game_map.explored |= self.game_map.visible
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
         for e in self.entities:
-            console.print(e.x, e.y, e.char, fg=e.color)
+            # Only show entities in the player's field of view
+            if self.game_map.visible[e.x, e.y]:
+                console.print(e.x, e.y, e.char, fg=e.color)
         context.present(console, integer_scaling=True)
         console.clear()
--- game_map.py
+++ game_map.py
@@ -14,11 +14,17 @@ class GameMap:
             fill_value=tile_types.wall,
             order="F",
         )
+        self.visible = np.full((width, height), fill_value=False, order="F")
+        self.explored = np.full((width, height), fill_value=False, order="F")
 
     def in_bounds(self, x: int, y: int) -> bool:
         """Return `True` if the point (x, y) is within the bounds of the map."""
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        """Render everything opaque to the console."""
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """Render everything visible or memorized to the console."""
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            [self.visible, self.explored],
+            [self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.FOG,
+        )
